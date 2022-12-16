3c54670699dff9e35e1f11ff166a449562160aed
Nino Carrillo <nino.carrillo@outlook.com>
2022-12-15 20:44:39 -0600

    rect 12

--- afsk-1200-ax25-rx.py
+++ afsk-1200-ax25-rx.py
@@ -143,36 +143,18 @@ def DecodeAX25(bitstream, verbose):
 
 
 def HighLowDetect(signal_value, detector):
-	#if (signal_value > detector['LastValue']):
-		#signal is increasing, check for new High
 	if signal_value > detector['High']:
-		#detector['High'] = signal_value
-		#detector['HighAttackRate'] = signal_value / detector['AttackPeriod']
-		#if detector['HighAttackRate'] == 0:
-			##detector['HighAttackRate'] = 1
 		detector['High'] = detector['High'] + detector['AttackRate']
 		if detector['High'] > signal_value:
 			detector['High'] = signal_value
 		detector['Midpoint'] = ((detector['High'] - detector['Low']) // 2) + detector['Low']
 		detector['HighSustainCount'] = 0
-		#detector['HighDecayRate'] = np.rint(detector['High'] / detector['DecayPeriod'])
-		#if detector['HighDecayRate'] == 0:
-			#detector['HighDecayRate'] = 1
-	#if (signal_value < detector['LastValue']):
-		#signal is decreasing, check for new Low
 	if signal_value < detector['Low']:
-		#detector['Low'] = signal_value
-		#detector['LowAttackRate'] = signal_value / detector['AttackPeriod']
-		#if detector['LowAttackRate'] == 0:
-			#detector['LowAttackRate'] = -1
 		detector['Low'] = detector['Low'] - detector['AttackRate']
 		if detector['Low'] < signal_value:
 			detector['Low'] = signal_value
 		detector['Midpoint'] = ((detector['High'] - detector['Low']) // 2) + detector['Low']
 		detector['LowSustainCount'] = 0
-		#detector['LowDecayRate'] = np.rint(detector['Low'] / detector['DecayPeriod'])
-		#if detector['LowDecayRate'] == 0:
-			#detector['LowDecayRate'] = -1
 	detector['LastValue'] = signal_value
 	if detector['HighSustainCount'] >= detector['SustainPeriod']:
 		detector['High'] = detector['High'] - detector['DecayRate']
@@ -216,12 +198,13 @@ def FilterDecimate(filter):
 			filter['DecimationCounter'] = 0
 			filter['DataBuffer'] = np.append(filter['DataBuffer'], np.array([data]))
 			filter['PeakDetector'] = PeakDetect(data, filter['PeakDetector'])
-			if filter['PeakDetector']['Envelope'] > 29490:
+			filter['GainChange'] = 0
+			if filter['PeakDetector']['Envelope'] > 24576:
 				filter['FilterShift'] = filter['FilterShift'] + 1
 				filter['PeakDetector']['Envelope'] = filter['PeakDetector']['Envelope'] / 2
 				if filter['FilterShift'] > 16:
 					filter['FilterShift'] = 16
-			if filter['PeakDetector']['Envelope'] < 9011:
+			if filter['PeakDetector']['Envelope'] < 8192:
 				filter['FilterShift'] = filter['FilterShift'] - 1
 				filter['PeakDetector']['Envelope'] = filter['PeakDetector']['Envelope'] * 2
 				if filter['FilterShift'] < -16:
@@ -237,6 +220,8 @@ def DemodulateAFSK(demodulator):
 
 	mark_sig = np.add(np.square(mark_cos_sig), np.square(mark_sin_sig))
 	mark_sig = np.rint(mark_sig / demodulator['SquareScale'])
+	# if mark_sig > 4095:
+	# 	print('mark clip')
 	mark_sig = np.clip(mark_sig, 0, demodulator['SquareClip'])
 
 	mark_sig = np.rint(demodulator['SquareOutputScale'] * np.sqrt(demodulator['SquareCoef'] * mark_sig))
@@ -246,15 +231,19 @@ def DemodulateAFSK(demodulator):
 
 	space_sig = np.add(np.square(space_cos_sig), np.square(space_sin_sig))
 	space_sig = np.rint(space_sig / demodulator['SquareScale'])
+	# if space_sig > 4095:
+	# 	print('space clip')
 	space_sig = np.clip(space_sig, 0, demodulator['SquareClip'])
 
 	space_sig = np.rint(demodulator['SquareOutputScale']* np.sqrt(demodulator['SquareCoef'] * space_sig))
+	space_sig = np.rint(space_sig * demodulator['SpaceRatio'])
 
 	demodulator['OutputFilterBuffer'] = demodulator['OutputFilterBuffer'][1:]
 	demodulator['OutputFilterBuffer'] = np.append(demodulator['OutputFilterBuffer'], np.array([mark_sig - space_sig]))
 
 	demodulator['Result'] = np.rint(np.convolve(demodulator['OutputFilterBuffer'], demodulator['OutputFilter'], 'valid') / pow(2, (16 + demodulator['OutputFilterShift'])))
-
+	demodulator['EnvelopeDetector'] = HighLowDetect(demodulator['Result'], demodulator['EnvelopeDetector'])
+	demodulator['Result'] = demodulator['Result'] - (demodulator['EnvelopeDetector']['Midpoint'] * 0.5)
 	return demodulator
 
 
@@ -325,8 +314,9 @@ def ProgDecodeAX25(decoder):
 
 
 def ProgSliceData(slicer):
-	slicer['EnvelopeDetector'] = HighLowDetect(slicer['NewSample'], slicer['EnvelopeDetector'])
-	slicer['Midpoint'] = slicer['EnvelopeDetector']['Midpoint'] * 0.66
+	# slicer['EnvelopeDetector'] = HighLowDetect(slicer['NewSample'], slicer['EnvelopeDetector'])
+	# slicer['Midpoint'] = np.rint(slicer['EnvelopeDetector']['Midpoint'] * 0.66)
+	slicer['Midpoint'] = 0
 	slicer['Result'] = np.array([])
 	slicer['PLLClock'] += slicer['PLLStep']
 	if slicer['PLLClock'] > ((slicer['PLLPeriod'] / 2.0) - 1.0):
@@ -360,6 +350,37 @@ print("Opened file. \r\nSample rate:", samplerate, "\r\nLength:", len(audio))
 
 # Filter and decimate the audio
 input_filter = np.array([77, 53, 58, 53, 39, 18, -4, -23, -38, -49, -61, -79, -106, -142, -180, -208, -215, -190, -130, -44, 52, 134, 181, 181, 137, 70, 11, -7, 39, 155, 317, 485, 604, 629, 534, 332, 67, -191, -368, -413, -315, -117, 94, 207, 124, -207, -765, -1451, -2099, -2516, -2534, -2058, -1096, 223, 1675, 2988, 3901, 4227, 3901, 2988, 1675, 223, -1096, -2058, -2534, -2516, -2099, -1451, -765, -207, 124, 207, 94, -117, -315, -413, -368, -191, 67, 332, 534, 629, 604, 485, 317, 155, 39, -7, 11, 70, 137, 181, 181, 134, 52, -44, -130, -190, -215, -208, -180, -142, -106, -79, -61, -49, -38, -23, -4, 18, 39, 53, 58, 53, 77])
+
+
+
+# /*
+#
+# FIR filter designed with
+# http://t-filter.appspot.com
+#
+# sampling frequency: 28800 Hz
+#
+# fixed point precision: 16 bits
+#
+# * 0 Hz - 300 Hz
+#   gain = 0
+#   desired attenuation = -70 dB
+#   actual attenuation = n/a
+#
+# * 1000 Hz - 2400 Hz
+#   gain = 1
+#   desired ripple = 2 dB
+#   actual ripple = n/a
+#
+# * 3600 Hz - 14400 Hz
+#   gain = 0
+#   desired attenuation = -70 dB
+#   actual attenuation = n/a
+#
+# */
+
+
+#input_filter = np.array([-20, -49, -93, -143, -184, -199, -171, -95, 20, 149, 262, 330, 340, 303, 247, 207, 213, 266, 338, 382, 347, 206, -22, -276, -471, -533, -441, -244, -62, -41, -294, -844, -1585, -2297, -2698, -2541, -1709, -272, 1507, 3229, 4476, 4931, 4476, 3229, 1507, -272, -1709, -2541, -2698, -2297, -1585, -844, -294, -41, -62, -244, -441, -533, -471, -276, -22, 206, 347, 382, 338, 266, 213, 207, 247, 303, 340, 330, 262, 149, 20, -95, -171, -199, -184, -143, -93, -49, -20])
 input_filter_buffer = np.zeros(len(input_filter))
 
 output_filter = np.array([583, 201, 157, 51, -113, -316, -525, -701, -796, -767, -580, -219, 309, 975, 1727, 2494, 3201, 3771, 4142, 4270, 4142, 3771, 3201, 2494, 1727, 975, 309, -219, -580, -767, -796, -701, -525, -316, -113, 51, 157, 201, 583])
@@ -380,9 +401,20 @@ correlator_taps = 12
 InputPeakDetector = {'AttackRate':50, 'SustainPeriod':3000, 'DecayRate':50, 'SustainCount':0, 'Envelope':0}
 FilterDecimator = {'Filter':input_filter, 'DecimationRate':decimation, 'FilterBuffer':input_filter_buffer, 'DataBuffer':np.array([]), 'PeakDetector':InputPeakDetector, 'FilterShift':0, 'DecimationCounter':0, 'NewSample':0}
 
+#generate the correlator window fuunction
+N = correlator_taps
+L = N + 1
+window =np.zeros(N)
+for n in range(N):
+	# window[n] = 1.0 - np.abs((n-(N/2)) / (L/2))
+
+	# this is the Hann window
+	window[n] = 0.5*(1-np.cos(2*np.pi*n/N))
+	window[n] = 1
+
 #set up the correlators
 
-mark_amp = 10000
+mark_amp = 15000
 f = 2200.0
 
 tstep = 1.0 / Fs
@@ -392,7 +424,6 @@ time = np.add(time, space_phase)
 space_amp = mark_amp
 space_cos = np.rint(space_amp * (np.cos(2 * f * np.pi * time)))
 space_sin = np.rint(space_amp * (np.sin(2 * f * np.pi * time)))
-print(space_sin)
 mark_phase = 0
 f = 1200.0
 time = np.arange(0, tstep * correlator_taps, tstep)
@@ -407,13 +438,27 @@ square_output_scale = 2.0
 square_coef = 4096.0
 square_clip = square_coef - 1.0
 
+space_cos = np.rint(np.multiply(space_cos,window))
+space_sin = np.rint(np.multiply(space_sin,window))
+mark_cos = np.rint(np.multiply(mark_cos,window))
+mark_sin = np.rint(np.multiply(mark_sin,window))
+print(mark_sin)
+print(space_sin)
+print(mark_cos)
+print(space_cos)
+print(window)
+
 correlator_buffer = np.zeros(correlator_taps)
 
-SlicerEnvelope = {'AttackRate':3, 'DecayRate':3, 'SustainPeriod':450, 'High':0, 'Low':0, 'HighSustainCount':0, 'LowSustainCount':0, 'Midpoint':0}
-AFSKDemodulator1 = {'MarkCOS':mark_cos, 'MarkSIN':mark_sin, 'SpaceCOS':space_cos, 'SpaceSIN':space_sin, 'OutputFilter':output_filter, 'OutputFilterBuffer':output_filter_buffer, 'NewSample':0, 'CorrelatorBuffer':correlator_buffer, 'CorrelatorShift':correlator_shift, 'SquareScale':square_scale, 'SquareClip':square_clip, 'SquareOutputScale':square_output_scale, 'SquareCoef':square_coef, 'Result':0, 'OutputFilterShift':output_filter_shift}
+SlicerEnvelope = {'AttackRate':1, 'DecayRate':1, 'SustainPeriod':450, 'High':0, 'Low':0, 'HighSustainCount':0, 'LowSustainCount':0, 'Midpoint':0}
+AFSKDemodulator1 = {'MarkCOS':mark_cos, 'MarkSIN':mark_sin, 'SpaceCOS':space_cos, 'SpaceSIN':space_sin, 'SpaceRatio':1.0, 'OutputFilter':output_filter, 'OutputFilterBuffer':output_filter_buffer, 'NewSample':0, 'CorrelatorBuffer':correlator_buffer, 'CorrelatorShift':correlator_shift, 'SquareScale':square_scale, 'SquareClip':square_clip, 'SquareOutputScale':square_output_scale, 'SquareCoef':square_coef, 'Result':0, 'OutputFilterShift':output_filter_shift, 'EnvelopeDetector':SlicerEnvelope}
+AFSKDemodulator2 = {'MarkCOS':mark_cos, 'MarkSIN':mark_sin, 'SpaceCOS':space_cos, 'SpaceSIN':space_sin, 'SpaceRatio':1.0, 'OutputFilter':output_filter, 'OutputFilterBuffer':output_filter_buffer, 'NewSample':0, 'CorrelatorBuffer':correlator_buffer, 'CorrelatorShift':correlator_shift, 'SquareScale':square_scale, 'SquareClip':square_clip, 'SquareOutputScale':square_output_scale, 'SquareCoef':square_coef, 'Result':0, 'OutputFilterShift':output_filter_shift, 'EnvelopeDetector':SlicerEnvelope}
 DataSlicer1 = {'Rate':0.7, 'PLLClock':0.0, 'PLLStep':1000000.0, 'PLLPeriod': 12.0 * 1000000, 'LastSample':0.0, 'NewSample':0.0,'Result':0.0, 'Midpoint':0, 'EnvelopeDetector':SlicerEnvelope}
+DataSlicer2 = {'Rate':0.7, 'PLLClock':0.0, 'PLLStep':1000000.0, 'PLLPeriod': 12.0 * 1000000, 'LastSample':0.0, 'NewSample':0.0,'Result':0.0, 'Midpoint':0, 'EnvelopeDetector':SlicerEnvelope}
 DifferentialDecoder1 = {'LastBit':0, 'NewBit':0, 'Result':0}
+DifferentialDecoder2 = {'LastBit':0, 'NewBit':0, 'Result':0}
 AX25Decoder1 = {'NewBit':0, 'BitIndex':0, 'Ones':0, 'ByteCount':0, 'WorkingByte':np.uint16(0), 'Result':np.array([]).astype('uint16'), 'CRC':np.array([]), 'PacketCount':0, 'Verbose':0, 'OutputTrigger':False}
+AX25Decoder2 = {'NewBit':0, 'BitIndex':0, 'Ones':0, 'ByteCount':0, 'WorkingByte':np.uint16(0), 'Result':np.array([]).astype('uint16'), 'CRC':np.array([]), 'PacketCount':0, 'Verbose':0, 'OutputTrigger':False}
 
 
 index1 = 0
@@ -453,9 +498,10 @@ for sample in audio:
 		index3 = index3 + 1
 		midpoint = DataSlicer1['Midpoint']
 		#print(index3, InputPeakDetector['Envelope'], space_sig_ratio, space_sig_gain_error)
-		print(f'{index3} midpoint: {midpoint}')
+		print(f'{index3}')
 	FilterDecimator['NewSample'] = sample
 	FilterDecimator = FilterDecimate(FilterDecimator)
+
 	for filtered_signal in FilterDecimator['DataBuffer']:
 		chop_filtered_audio_buffer = np.append(chop_filtered_audio_buffer, np.array([filtered_signal]))
 		AFSKDemodulator1['NewSample'] = filtered_signal
@@ -489,30 +535,11 @@ for sample in audio:
 					chop_demodulated_audio_buffer = np.array([])
 					scipy.io.wavfile.write(dirname+filename+'-filtered.wav', Fs, chop_filtered_audio_buffer.astype(np.int16))
 					chop_filtered_audio_buffer = np.array([])
-				# if AX25Decoder1['CRC'][1] == 1:
-				# 	print(AX25Decoder1[Result])
-# scipy.io.wavfile.write(dirname+"PeakDetect.wav", round(samplerate / decimation), envelope.astype(np.int16))
-# scipy.io.wavfile.write(dirname+"FilteredSignal.wav", round(samplerate / decimation), filtered_signal_buffer.astype(np.int16))
 
-# scipy.io.wavfile.write(dirname+"MarkCorrelatorSignal.wav", round(samplerate / decimation), mark_correlator_buffer.astype(np.int16))
-# scipy.io.wavfile.write(dirname+"SpaceCorrelatorSignal.wav", round(samplerate / decimation), space_correlator_buffer.astype(np.int16))
+
+
 scipy.io.wavfile.write(dirname+"DemodSignal.wav", round(samplerate / decimation), demod_sig_buffer.astype(np.int16))
 
-# scipy.io.wavfile.write(dirname+"SpaceGain.wav", round(samplerate / decimation), space_gain_buffer.astype(np.int16))
-
-# scipy.io.wavfile.write(dirname+"SpaceEnvelope.wav", round(samplerate / decimation), space_envelope_buffer.astype(np.int16))
-
-# scipy.io.wavfile.write(dirname+"MarkEnvelope.wav", round(samplerate / decimation), mark_envelope_buffer.astype(np.int16))
-
-#data = SliceData(demod_sig_buffer, 0.7, 12)
-#print("Length of sliced data:", len(data))
-#print(data[10000:10100])
-# file = open('demod_output.txt', 'w')
-# np.savetxt(file, data.astype(np.int16))
-# file.close()
-#data = DifferentialDecode(data)
-#count = DecodeAX25(data, 1)
-#print("Decoded packet count:", count)
 
 print('made new directory: ', dirname)
 print('done')
