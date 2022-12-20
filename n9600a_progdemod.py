ae21b8a87d532ae49d2210dd84bf130b9aa0f02e
Nino Carrillo <nino.carrillo@outlook.com>
2022-12-20 09:51:47 -0500

    adjust sqrt scaling

--- n9600a_progdemod.py
+++ n9600a_progdemod.py
@@ -38,7 +38,7 @@ def InitAFSKDemod(demodulator):
 	demodulator['MarkCOS'] = np.rint(demodulator['MarkAmplitude'] * (np.cos(2 * demodulator['MarkFreq'] * np.pi * time)))
 	demodulator['MarkSIN'] = np.rint(demodulator['MarkAmplitude'] * (np.sin(2 * demodulator['MarkFreq'] * np.pi * time)))
 	demodulator['CorrelatorShift'] = 0
-	demodulator['SquareScale'] = 2**(31 - demodulator['SqrtBitCount'])
+	demodulator['SquareScale'] = 2**(30 - demodulator['SqrtBitCount'])
 	demodulator['SquareCoef'] = 2**demodulator['SqrtBitCount']
 	demodulator['SquareClip'] = demodulator['SquareCoef'] - 1
 	demodulator['SqrtTable'] = np.zeros(demodulator['SquareCoef'])
