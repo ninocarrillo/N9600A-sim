88ba9c5baaceb56a267c4944fbf0bc8a14568785
Nino Carrillo <nino.carrillo@outlook.com>
2023-09-04 18:32:33 -0400

    hamming generator

--- format_output.py
+++ format_output.py
@@ -18,3 +18,17 @@ def GenInt16ArrayC(name, array, column_width):
 			result += ','
 	result += ' };'
 	return result
+
+def GenInt16ArrayHexC(name, array, column_width):
+	result = '\n'
+	result += f'const __prog__ int16_t __attribute__((space(prog))) {name}[{len(array)}] = '
+	result += '{ '
+	y = len(array)
+	for x in range(y):
+		if x % column_width == 0:
+			result += ' \\\n     '
+		result += f' {hex(int(np.rint(array[x])))}'
+		if x < (y-1):
+			result += ','
+	result += ' };'
+	return result
--- hamming.py
+++ hamming.py
@@ -1,4 +1,7 @@
+import sys
+import os
 import numpy
+import format_output as fo
 def Hamming74Encode(input_data):
 	data_count = 4
 	output = 0
@@ -11,9 +14,9 @@ def Hamming74Encode(input_data):
 	
 	even = 0
 	
-	parity_bits[0] = data_bits[0] + data_bits[1]                + data_bits[3] + even
-	parity_bits[1] = data_bits[0]                + data_bits[2] + data_bits[3] + even
-	parity_bits[2] =                data_bits[1] + data_bits[2] + data_bits[3] + even
+	parity_bits[2] = data_bits[0] + data_bits[1] + data_bits[2]                + even
+	parity_bits[1] = data_bits[0] + data_bits[1]                + data_bits[3] + even
+	parity_bits[0] = data_bits[0]                + data_bits[2] + data_bits[3] + even
 	
 	interleaved_data = numpy.zeros(data_count + parity_count)
 	sequential_data = numpy.zeros(data_count + parity_count)
@@ -42,9 +45,73 @@ def Hamming74Encode(input_data):
 	for index in range(data_count + parity_count):
 		sequential_codeword = sequential_codeword + int(sequential_data[index] * (2**index))
 	
-	return sequential_codeword, interleaved_codeword
+	return sequential_codeword
+	
+def Hamming74Decode(input_data):
+	valid_codewords = numpy.zeros(16)
+	for index in range(16):
+		valid_codewords[index] = Hamming74Encode(index)
+	
+	result = numpy.zeros(16)
+	for index in range(16):
+		product = int(input_data) ^ int(valid_codewords[index])
+		distance = 0
+		for bit_index in range(8):
+			if product & 1:
+				distance = distance + 1
+			product = product >> 1
+		result[index] = distance
+	
+	bit_distance = numpy.amin(result)	
+	result = numpy.argmin(result) & 0xF
+	
+	return [result, bit_distance]
 
+encode_table = numpy.zeros(16)
 
 for data in range(16):
-	codewords = Hamming74Encode(data)
-	print(f's: {hex(codewords[0])}    i:{hex(codewords[1])}')
+	encode_table[data] = Hamming74Encode(data)
+	
+decode_table = numpy.zeros(128)
+distance_table = numpy.zeros(128)
+for data in range(128):
+	result = Hamming74Decode(data)
+	decode_table[data] = result[0]
+	distance_table[data] = result[1]
+	
+#generate a new directory for the report
+run_number = 0
+print('trying to make a new directory')
+while True:
+	run_number = run_number + 1
+	dirname = f'./run{run_number}/'
+	try:
+		os.mkdir(dirname)
+	except:
+		print(dirname + ' exists')
+		continue
+	break
+print(f'made directory {dirname}')
+# Generate and save report file
+report_file_name = f'run{run_number}_report.txt'
+try:
+	report_file = open(dirname + report_file_name, 'w+')
+except:
+	print('Unable to create report file.')
+with report_file:
+	report_file.write('# Command line: ')
+	for argument in sys.argv:
+		report_file.write(f'{argument} ')
+
+
+	report_file.write('\n\n# Hamming(7,4) Encoding Table')
+	report_file.write(fo.GenInt16ArrayHexC(f'Hamming74Encode', encode_table, 1))
+
+	report_file.write('\n\n# Hamming(7,4) Decoding Table')
+	report_file.write(fo.GenInt16ArrayHexC(f'Hamming74Decode', decode_table, 8))
+
+	report_file.write('\n\n# Hamming(7,4) Distance Table')
+	report_file.write(fo.GenInt16ArrayC(f'Hamming74Distance', distance_table, 8))
+
+
+	report_file.close()
