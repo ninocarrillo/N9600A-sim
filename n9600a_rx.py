66c7b44c4129af5a98f920787c676c6a8555797e
Nino Carrillo <nino.carrillo@outlook.com>
2023-11-07 20:16:46 -0500

    parameter iterator

--- n9600a_rx.py
+++ n9600a_rx.py
@@ -15,6 +15,7 @@ import n9600a_bpsk as bpsk
 import n9600a_qpsk as qpsk
 import n9600a_qpsk32 as qpsk32
 import n9600a_bpsk32 as bpsk32
+import numpy as np
 
 if len(sys.argv) < 3:
 	print("Not enough arguments. Usage: py -3 n9600a_rx.py <ini file> <wav file>")
@@ -53,48 +54,149 @@ except:
 	print(f'{sys.argv[1]} [General] \'{key_string}\' is missing or invalid')
 	sys.exit(-2)
 
+key_string = "iterators"
+try:
+	state[f'{key_string}'] = int(config[f'General'][f'{key_string}'])
+except:
+	print(f'{sys.argv[1]} [General] \'{key_string}\' is missing or invalid')
+	sys.exit(-2)
+
 
 state['argv'] = sys.argv
 state['config'] = config
 
-if DemodulatorType == 'afsk':
-	afsk.FullProcess(state)
+if state['iterators'] > 0:
+	print('Iterators Active.')
+	IteratorStructure = []
+	for iterator_number in range(state['iterators'] + 1):
+		print(f'Reading settings for Iterator {iterator_number}')
+		IteratorStructure.append({})
+		if config.has_section(f'Iterator {iterator_number}'):
+			key_string = "section"
+			try:
+				IteratorStructure[iterator_number][f'{key_string}'] = config[f'Iterator {iterator_number}'][f'{key_string}']
+			except:
+					print(f'{sys.argv[1]} [f\'Iterator {iterator_number}\'] \'{key_string}\' is missing or invalid')
+					sys.exit(-2)
+			key_string = "key name"
+			try:
+				IteratorStructure[iterator_number][f'{key_string}'] = config[f'Iterator {iterator_number}'][f'{key_string}']
+			except:
+					print(f'{sys.argv[1]} [f\'Iterator {iterator_number}\'] \'{key_string}\' is missing or invalid')
+					sys.exit(-2)
+			key_string = "low value"
+			try:
+				IteratorStructure[iterator_number][f'{key_string}'] = float(config[f'Iterator {iterator_number}'][f'{key_string}'])
+			except:
+					print(f'{sys.argv[1]} [f\'Iterator {iterator_number}\'] \'{key_string}\' is missing or invalid')
+					sys.exit(-2)
+			key_string = "high value"
+			try:
+				IteratorStructure[iterator_number][f'{key_string}'] = float(config[f'Iterator {iterator_number}'][f'{key_string}'])
+			except:
+					print(f'{sys.argv[1]} [f\'Iterator {iterator_number}\'] \'{key_string}\' is missing or invalid')
+					sys.exit(-2)
+			key_string = "step value"
+			try:
+				IteratorStructure[iterator_number][f'{key_string}'] = float(config[f'Iterator {iterator_number}'][f'{key_string}'])
+			except:
+					print(f'{sys.argv[1]} [f\'Iterator {iterator_number}\'] \'{key_string}\' is missing or invalid')
+					sys.exit(-2)
+			IteratorStructure[iterator_number]['current value'] = IteratorStructure[iterator_number]['low value']
+			IteratorStructure[iterator_number]['cycles'] = np.rint((IteratorStructure[iterator_number]['high value'] - IteratorStructure[iterator_number]['low value']) / IteratorStructure[iterator_number]['step value'])
 
-elif DemodulatorType == 'dpsk':
-	dpsk.FullProcess(state)
 
-elif DemodulatorType == 'dpsk2':
-	dpsk2.FullProcess(state)
+	iterating = True
+	while iterating:
+		for iterator_number in range(1, state['iterators'] + 1):
+			config[f"{IteratorStructure[iterator_number]['section']}"][f"{IteratorStructure[iterator_number]['key name']}"] = str(IteratorStructure[iterator_number]['current value'])
+			print(f"{IteratorStructure[iterator_number]['section']} {IteratorStructure[iterator_number]['key name']} {IteratorStructure[iterator_number]['current value']}")
+		state['config'] = config
 
-elif DemodulatorType == 'gfsk':
-	gfsk.FullProcess(state)
+		if DemodulatorType == 'afsk':
+			afsk.FullProcess(state)
 
-elif DemodulatorType == 'gfsk2':
-	gfsk2.FullProcess(state)
+		elif DemodulatorType == 'dpsk':
+			dpsk.FullProcess(state)
 
-elif DemodulatorType == 'ncotest':
-	nco.Test(state)
+		elif DemodulatorType == 'dpsk2':
+			dpsk2.FullProcess(state)
 
-elif DemodulatorType == 'dpsk3':
-	dpsk3.FullProcess(state)
+		elif DemodulatorType == 'gfsk':
+			gfsk.FullProcess(state)
 
-elif DemodulatorType == 'dpsk4':
-	dpsk4.FullProcess(state)
+		elif DemodulatorType == 'gfsk2':
+			gfsk2.FullProcess(state)
 
-elif DemodulatorType == 'iirtest':
-	filters.IIRTest(state)
+		elif DemodulatorType == 'ncotest':
+			nco.Test(state)
 
-elif DemodulatorType == 'rrcfsk':
-	rrcfsk.FullProcess(state)
+		elif DemodulatorType == 'dpsk3':
+			dpsk3.FullProcess(state)
 
-elif DemodulatorType== 'bpsk':
-	bpsk.FullProcess(state)
+		elif DemodulatorType == 'dpsk4':
+			dpsk4.FullProcess(state)
 
-elif DemodulatorType == 'qpsk':
-	qpsk.FullProcess(state)
+		elif DemodulatorType == 'iirtest':
+			filters.IIRTest(state)
 
-elif DemodulatorType == 'qpsk32':
-	qpsk32.FullProcess(state)
+		elif DemodulatorType == 'rrcfsk':
+			rrcfsk.FullProcess(state)
 
-elif DemodulatorType == 'bpsk32':
-	bpsk32.FullProcess(state)
+		elif DemodulatorType== 'bpsk':
+			bpsk.FullProcess(state)
+
+		elif DemodulatorType == 'qpsk':
+			qpsk.FullProcess(state)
+
+		elif DemodulatorType == 'qpsk32':
+			qpsk32.FullProcess(state)
+
+		elif DemodulatorType == 'bpsk32':
+			bpsk32.FullProcess(state)
+		iterating = False
+
+
+else:
+	# No iterators, just run once.
+	if DemodulatorType == 'afsk':
+		afsk.FullProcess(state)
+
+	elif DemodulatorType == 'dpsk':
+		dpsk.FullProcess(state)
+
+	elif DemodulatorType == 'dpsk2':
+		dpsk2.FullProcess(state)
+
+	elif DemodulatorType == 'gfsk':
+		gfsk.FullProcess(state)
+
+	elif DemodulatorType == 'gfsk2':
+		gfsk2.FullProcess(state)
+
+	elif DemodulatorType == 'ncotest':
+		nco.Test(state)
+
+	elif DemodulatorType == 'dpsk3':
+		dpsk3.FullProcess(state)
+
+	elif DemodulatorType == 'dpsk4':
+		dpsk4.FullProcess(state)
+
+	elif DemodulatorType == 'iirtest':
+		filters.IIRTest(state)
+
+	elif DemodulatorType == 'rrcfsk':
+		rrcfsk.FullProcess(state)
+
+	elif DemodulatorType== 'bpsk':
+		bpsk.FullProcess(state)
+
+	elif DemodulatorType == 'qpsk':
+		qpsk.FullProcess(state)
+
+	elif DemodulatorType == 'qpsk32':
+		qpsk32.FullProcess(state)
+
+	elif DemodulatorType == 'bpsk32':
+		bpsk32.FullProcess(state)
