6aaf9286b1c3382e5afde11824b33bea417f27a0
Nino Carrillo <nino.carrillo@outlook.com>
2024-01-01 19:47:30 -0500

    qpsk32 rewrite

--- n9600a_qpsk32.py
+++ n9600a_qpsk32.py
@@ -194,13 +194,6 @@ def GetQPSKDemodConfig(config, num, id_string):
 		print(f'{sys.argv[1]} [{id_string}{num}] \'{key_string}\' is missing or invalid')
 		sys.exit(-2)
 
-	key_string = "loop filter gain"
-	try:
-		this['LoopFilter'][f'{key_string}'] = float(config[f'{id_string}{num}'][f'{key_string}'])
-	except:
-		print(f'{sys.argv[1]} [{id_string}{num}] \'{key_string}\' is missing or invalid')
-		sys.exit(-2)
-
 	key_string = "loop integrator trim"
 	try:
 		this['LoopFilter'][f'{key_string}'] = int(config[f'{id_string}{num}'][f'{key_string}'])
@@ -435,7 +428,7 @@ def FullProcess(state):
 			# Check for uniqueness
 			total_packets += 1
 			CRC = AX25Decoder['CRC'][0]
-			filename = f'Packet-{total_packets}_CRC-{format(CRC,"#06x")}_Index-{index}'
+			filename = f'Packet-{total_packets}_CRC-{format(CRC,"#06x")}'
 			print(f'{filename}')
 			if state['reports'] == True:
 				try:
@@ -460,17 +453,12 @@ def FullProcess(state):
 		scipy.io.wavfile.write(dirname+"I_Mixer.wav", FilterDecimator['OutputSampleRate'], QPSKDemodulator['I_Mixer'].astype(np.int16))
 		scipy.io.wavfile.write(dirname+"Q_Mixer.wav", FilterDecimator['OutputSampleRate'], QPSKDemodulator['Q_Mixer'].astype(np.int16))
 
-
-	#print(PulseFilter['Taps'])
-
 	if state['plots'] == True:
 		plt.figure()
 		plt.subplot(221)
-		#plt.plot(FilterDecimator['FilterBuffer'])
 		plt.plot(FilterDecimator['FilterBuffer'])
 		plt.plot(QPSKDemodulator['I_LPFOutput'])
 		plt.plot(FilterDecimator['EnvelopeBuffer'])
-		#plt.plot(QPSKDemodulator[1]['Q_LPFOutput'])
 		plt.title('I LPF Output')
 		plt.legend(['Filtered Input','I_LPF Output','Envelope'])
 		plt.subplot(222)
@@ -525,148 +513,3 @@ def FullProcess(state):
 			report_file.close()
 
 	return total_packets
-
-
-
-
-
-def ModulateRRC(state):
-	argv = state['argv']
-	config = state['config']
-	print(f'Started Shaped QPSK Modulator process')
-	print(f'Reading settings for RRC Pulse Shaping Filter')
-	#PulseFilter = pulse_filter.GetGaussFilterConfig(state)
-	#PulseFilter = pulse_filter.InitGaussFilter(PulseFilter)
-	PulseFilter = pulse_filter.GetRRCFilterConfig(state)
-	PulseFilter = pulse_filter.InitRRCFilter(PulseFilter)
-	PulseFilter['SymbolMap'] = pulse_filter.GetSymbolMapConfig(state)
-	NCO = nco.GetNCOConfig(config, 1, "TX NCO ")
-	NCO = nco.InitNCO(NCO)
-
-	PulseFilter = pulse_filter.GenPulseFilterPatterns(PulseFilter)
-	print(max(PulseFilter['FilterPatterns']))
-	plt.plot(PulseFilter['FilterPatterns'])
-	plt.show()
-
-	#BitStream = pulse_filter.ExpandSampleStream(state['InputData'], PulseFilter)
-	BitStream = pulse_filter.BytesToSymbols(state['InputData'], PulseFilter)
-	plt.figure()
-	plt.plot(BitStream)
-	plt.title('BitStream')
-	plt.show()
-	#ModulatingWaveform = np.convolve(PulseFilter['Taps'], BitStream)
-	#ModAmplitude = 64
-	#ModulatingWaveform = np.rint(ModAmplitude * ModulatingWaveform / max(ModulatingWaveform))
-	ModulatingWaveform = np.zeros(len(BitStream) * PulseFilter['Oversample'] * PulseFilter['undersample'])
-
-	i = 0
-	shift_register = int(0)
-	filter_mask = (2**PulseFilter['symbol span']) - 1
-	for bit in BitStream:
-		shift_register <<= 1
-		if bit == 1:
-			shift_register |= 1
-		shift_register = shift_register & filter_mask
-		for phase in range(PulseFilter['Oversample']):
-			for subphase in range(PulseFilter['undersample']):
-				ModulatingWaveform[i] = PulseFilter['FilterPatterns'][(shift_register * PulseFilter['Oversample']) + phase]
-				i += 1
-
-	plt.figure()
-	plt.plot(ModulatingWaveform)
-	plt.title('Modulating Waveform')
-	plt.show()
-
-	Baseband = np.zeros(len(ModulatingWaveform))
-	i = 0
-	for Amplitude in ModulatingWaveform:
-		NCO = nco.UpdateNCO(NCO)
-		Baseband[i] = Amplitude * NCO['Sine'] // PulseFilter['amplitude']
-		i += 1
-
-	plt.figure()
-	plt.plot(Baseband)
-	plt.title('Baseband')
-	plt.show()
-
-	plt.figure()
-	plt.subplot(221)
-	plt.plot(PulseFilter['Time'], PulseFilter['Taps'], 'b')
-	#plt.plot(PulseFilter['Time'], PulseFilter['RC'], 'r')
-	plt.xticks(PulseFilter['SymbolTicks'])
-	plt.xticks(color='w')
-	plt.grid(True)
-
-	plt.subplot(222)
-	plt.plot(ModulatingWaveform, 'b')
-
-	eye_data = pulse_filter.GenEyeData2(ModulatingWaveform, PulseFilter['Oversample'], 0)
-	plt.subplot(223)
-	plt.plot(eye_data)
-
-
-	fft_n = len(ModulatingWaveform)
-	x = np.linspace(0.0, fft_n * PulseFilter['TimeStep'], fft_n, endpoint = False)
-	x_fft = fftfreq(fft_n, PulseFilter['TimeStep'])[:fft_n//2]
-	ModulatingWaveform_fft = fft(ModulatingWaveform)
-	ModulatingWaveform_fft = fft(ModulatingWaveform)
-	fft_max = max(abs(ModulatingWaveform_fft))
-	ModulatingWaveform_fft = ModulatingWaveform_fft / fft_max
-	plt.subplot(224)
-	plt.plot(x_fft, 10*np.log(np.abs(ModulatingWaveform_fft[0:fft_n//2])))
-	plt.xlim(0,PulseFilter['symbol rate'] * 4)
-	plt.ylim(-100,10)
-	plt.show()
-
-	#generate a new directory for the reports
-	run_number = 0
-	print('trying to make a new directory')
-	while True:
-		run_number = run_number + 1
-		dirname = f'./run{run_number}/'
-		try:
-			os.mkdir(dirname)
-		except:
-			print(dirname + ' exists')
-			continue
-		break
-
-	ModulatingWaveform = ModulatingWaveform / max(ModulatingWaveform)
-	ModulatingWaveform = ModulatingWaveform * 32767
-	scipy.io.wavfile.write(dirname+"ModSignal.wav", PulseFilter['sample rate'], ModulatingWaveform.astype(np.int16))
-	scipy.io.wavfile.write(dirname+"Baseband.wav", PulseFilter['sample rate'], Baseband.astype(np.int16))
-
-
-	# Generate and save report file
-	report_file_name = f'run{run_number}_report.txt'
-	try:
-		report_file = open(dirname + report_file_name, 'w+')
-	except:
-		print('Unable to create report file.')
-	with report_file:
-		report_file.write('# Command line: ')
-		for argument in sys.argv:
-			report_file.write(f'{argument} ')
-		report_file.write('\n#\n########## Begin Transcribed .ini file: ##########\n')
-		try:
-			ini_file = open(sys.argv[1])
-		except:
-			report_file.write('Unable to open .ini file.')
-		with ini_file:
-			for character in ini_file:
-				report_file.write(character)
-
-		report_file.write('\n\n########## End Transcribed .ini file: ##########\n')
-
-		report_file.write(fo.GenInt16ArrayC('RRCFilter', PulseFilter['Taps'] * np.rint(65536 / np.sum(PulseFilter['Taps'])), PulseFilter['Oversample']))
-		print(np.sum(PulseFilter['Taps']))
-
-
-		report_file.write('\n')
-		report_file.write(fo.GenInt16ArrayC(f'QPSKFilterPatterns', PulseFilter['FilterPatterns'], PulseFilter['Oversample']))
-		report_file.write('\n\n')
-
-		report_file.close()
-
-
-	return
