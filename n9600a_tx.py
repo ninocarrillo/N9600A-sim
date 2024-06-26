91c2222ec0c9b36648c37326e31104fddf3e1fdd
Nino Carrillo <nino.carrillo@outlook.com>
2024-06-26 11:50:28 -0400

    udpate

--- n9600a_tx.py
+++ n9600a_tx.py
@@ -40,7 +40,7 @@ state = {}
 state['argv'] = sys.argv
 state['config'] = config
 
-data_count = 2000
+data_count = 10000
 np.random.seed(0)
 state['InputData'] = np.random.randint(0,256, data_count)
 # try:
